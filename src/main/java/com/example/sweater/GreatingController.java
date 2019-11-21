ff1fd5fd44af5c3efce3a236d3f87e430effe20f
kuzinale <kuzi4kin@gmail.com>
2019-11-22 00:04:08 +0300

    Database added.

--- src/main/java/com/example/sweater/GreatingController.java
+++ src/main/java/com/example/sweater/GreatingController.java
@@ -5,12 +5,10 @@ import com.example.sweater.domain.Message;
 import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
