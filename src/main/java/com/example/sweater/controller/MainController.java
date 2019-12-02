ec3518697ea2602ca3010b7514866d76a899c253
kuzinale <kuzi4kin@gmail.com>
2019-12-02 14:57:59 +0300

    after delete /filter

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -7,6 +7,7 @@ import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,11 +23,18 @@ public class MainController {
     public String greeting(String name, Map<String,Object> model) {
          return "greeting";
     }
+
     @GetMapping("/main")
-    public String main(Map<String,Object> model)
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model)
     {
         Iterable<Message> messages= messageRepo.findAll();
-        model.put("messages",messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }else {
+            messages = messageRepo.findAll();
+        }
+        model.addAttribute("messages",messages);
+        model.addAttribute("filter", filter);
         return "main";
 
     }
@@ -38,12 +46,11 @@ public class MainController {
             @RequestParam String tag, Map<String,Object> model) {
         Message message = new Message(text,tag, user);
         messageRepo.save(message);
-
         Iterable<Message> messages= messageRepo.findAll();
         model.put("messages",messages);
         return "main";
     }
-    @PostMapping("/filter")
+  /*  @PostMapping("/filter")
     public String filter(@RequestParam String filter, Map<String,Object> model){
         Iterable<Message> messages;
         if (filter != null && !filter.isEmpty()) {
@@ -53,5 +60,5 @@ public class MainController {
         }
        model.put("messages", messages);
      return "main";
-    }
+    }*/
 }
